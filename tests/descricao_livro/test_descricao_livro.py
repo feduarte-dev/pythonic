aafeaa504be6e5613803c3f53278befd659738d1
Felipe Duarte <feduarte.dev@gmail.com>
2024-04-15 11:00:31 -0300

    requisito 4

--- tests/descricao_livro/test_descricao_livro.py
+++ tests/descricao_livro/test_descricao_livro.py
@@ -1,5 +1,11 @@
-# from src.livro.livro import Livro
+from src.livro.livro import Livro
 
 
-def test_descricao_livro():
-    pass  # Colocar sua implementação aqui
+def test_descricao_livro(capsys):
+    livro = Livro("Test title", "Test author", 200)
+    print(livro)
+    captured = capsys.readouterr()
+    assert (
+        captured.out
+        == "O livro Test title de Test author possui 200 páginas.\n"
+    )
