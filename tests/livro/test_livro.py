6245c69fd73a4d29044919162647231824be720c
Felipe Duarte <feduarte.dev@gmail.com>
2024-04-15 10:55:41 -0300

    requisito 3

--- tests/livro/test_livro.py
+++ tests/livro/test_livro.py
@@ -1,5 +1,8 @@
-# from src.livro.livro import Livro
+from src.livro.livro import Livro
 
 
 def test_cria_livro():
-    pass  # Seu teste deve ser escrito aqui
+    livro = Livro("Test title", "Test author", 200)
+    assert livro.titulo == "Test title"
+    assert livro.autor == "Test author"
+    assert livro.paginas == 200
